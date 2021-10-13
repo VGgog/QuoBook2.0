602be8180edb8f9f9a327cda0a9fc66ea8e00090
Aidar <gluhovkich@gmail.com>
2021-10-13 22:31:58 +0300

    Add a ful registration and save data in db

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, db
 from flask import render_template, flash, redirect
 from app.forms import LoginForm
+from app.models import Users
 
 
 @app.route('/')
@@ -18,6 +19,12 @@ def docs():
 def reg():
     login = LoginForm()
     if login.validate_on_submit():
+        if Users.query.filter_by(username=login.username.data).first():
+            flash('Пользователь под таким логином уже существует.')
+            return redirect('/registration')
+        user = Users(username=login.username.data, password_hash=Users.make_password_hash(login.password.data))
+        db.session.add(user)
+        db.session.commit()
         flash('Вы успешно зарегистрировались.')
         return redirect('/registration')
     return render_template('registration.html', title='Registration', form=login)
