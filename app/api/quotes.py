2b3ec801a2102d4286797bb25e48e19856b21e91
Aidar <gluhovkich@gmail.com>
2021-10-15 23:29:34 +0300

    Add a Post methods

--- app/api/quotes.py
+++ app/api/quotes.py
@@ -1,6 +1,7 @@
 from app.api import bp
-from flask import jsonify
-from app.models import Quote
+from flask import jsonify, request
+from app.models import Quote, Users
+from app import db
 
 
 @bp.route('/quotes/quote/<int:quote_id>', methods=['GET'])
@@ -14,3 +15,45 @@ def send_quotes(quote_id):
         'quote': info_for_quote['quote']
     }
     return jsonify(quote)
+
+
+@bp.route('/quote/new_quote', methods=['POST'])
+def add_new_quote():
+    quote_data = request.get_json()
+    if 'login' and 'password' and 'quote' in quote_data:
+
+        if Users.query.filter_by(username=quote_data['login']).first():
+
+            password_hash = Users.query.filter_by(username=quote_data['login']).first().__dict__['password_hash']
+            user_id = Users.query.filter_by(username=quote_data['login']).first().__dict__['user_id']
+            if Users.check_password(password_hash, quote_data['password']):
+
+                info_for_quote = quote_data['quote']
+                if 'author' and 'book_title' and 'quote' in info_for_quote:
+                    count = Quote.query.count()
+                    quote = Quote(user_id=user_id, quote_id=count + 1,  author=info_for_quote['author'],
+                                  book_title=info_for_quote['book_title'], quote=info_for_quote['quote'])
+                    db.session.add(quote)
+                    db.session.commit()
+
+                    #info_for_add_quote = Quote.query.filter_by(quote=info_for_quote['quote']).first().__dict__
+                    user_id = Quote.query.filter_by(quote=info_for_quote['quote']).first().__dict__['user_id']
+                    quote_id = Quote.query.filter_by(quote=info_for_quote['quote']).first().__dict__['quote_id']
+                    author = Quote.query.filter_by(quote=info_for_quote['quote']).first().__dict__['author']
+                    book_title = Quote.query.filter_by(quote=info_for_quote['quote']).first().__dict__['book_title']
+                    quote = Quote.query.filter_by(quote=info_for_quote['quote']).first().__dict__['quote']
+                    new_quote = {
+                        "user_id": user_id,
+                        "quote_id": quote_id,
+                        "quote": {
+                            "author": author,
+                            "book_title": book_title,
+                            "quote": quote
+                        }
+                    }
+                    return jsonify(new_quote)
+
+                return "The form of the submitted json is not correct.", 404
+            return "The specified password is incorrect", 404
+        return "There is no user with this login.", 404
+    return "The form of the submitted json is not correct.", 404
