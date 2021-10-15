5c7b84136b4311800c0ac590fd7f1d3d9cc645d1
Aidar <gluhovkich@gmail.com>
2021-10-15 16:21:18 +0300

    Add a initialization api

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+app.config['JSON_AS_ASCII'] = False
+
+
+from app.api import bp as api_bp
+app.register_blueprint(api_bp, url_prefix='/api')
 
 
 from app import routes, models
