927bf0d062652d0239968679dcdced1a33dac5c9
Aidar <gluhovkich@gmail.com>
2021-10-18 16:09:32 +0300

    Reworked getting data from the received Object

--- app/api/func.py
+++ app/api/func.py
@@ -3,11 +3,11 @@
 def translates_into_the_correct_format(info_for_quote):
     """Переводит цитату в правильный формат"""
     return {
-        'user_id': info_for_quote['user_id'],
-        'quote_id': info_for_quote['quote_id'],
+        'user_id': info_for_quote.user_id,
+        'quote_id': info_for_quote.quote_id,
         'quote': {
-            'author': info_for_quote['author'],
-            'book_title': info_for_quote['book_title'],
-            'quote': info_for_quote['quote']
+            'author': info_for_quote.author,
+            'book_title': info_for_quote.book_title,
+            'quote': info_for_quote.quote
         }
     }
