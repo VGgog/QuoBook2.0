c841706c68c235624ca384cc42cc0b5c4e9b2ca0
Aidar <gluhovkich@gmail.com>
2021-10-20 22:10:45 +0300

    Refactor POST-methods

--- app/api/quotes.py
+++ app/api/quotes.py
@@ -48,24 +48,18 @@ def send_give_count_quotes(count):
 def add_new_quote():
     """Добавляет новую цитату"""
     quote_data = request.get_json()
-    if 'login' and 'password' and 'quote' in quote_data:
+    if func.checking_correctness_json(quote_data):
         if func.check_user(quote_data):
-
             info_for_quote = quote_data['quote']
-            if 'author' and 'book_title' and 'quote' in info_for_quote:
+            # Проверяет наличие цитаты в бд.
+            if not Quote.query.filter_by(quote=info_for_quote['quote']).first():
+                quote = func.made_quote_obj(quote_data, quote_id=Quote.query.count()+1)
 
-                # Проверяет наличие цитаты в бд.
-                if not Quote.query.filter_by(quote=info_for_quote['quote']).first():
-                    user_id = Users.query.filter_by(username=quote_data['login']).first().user_id
-                    quote = Quote(user_id=user_id, quote_id=Quote.query.count() + 1,  author=info_for_quote['author'],
-                                  book_title=info_for_quote['book_title'], quote=info_for_quote['quote'])
-                    db.session.add(quote)
-                    db.session.commit()
-                    return jsonify(func.translates_into_the_correct_format(
-                        Quote.query.filter_by(quote=info_for_quote['quote']).first()))
-
-                return "This quote already added"
-            return "The form of the submitted json is not correct.", 400
+                db.session.add(quote)
+                db.session.commit()
+                return jsonify(func.translates_into_the_correct_format(
+                    Quote.query.filter_by(quote=info_for_quote['quote']).first()))
+            return "This quote already added"
         return "Login or password is incorrect", 401
     return "The form of the submitted json is not correct.", 400
 
@@ -82,7 +76,7 @@ def add_or_update_quote():
                 if Quote.query.filter_by(quote_id=quote_id).first():
                     db.session.delete(Quote.query.filter_by(quote_id=quote_id).first())
 
-                quote = func.made_quote_obj(quote_data)
+                quote = func.made_quote_obj(quote_data, quote_id)
                 db.session.add(quote)
                 db.session.commit()
                 return jsonify(func.translates_into_the_correct_format(quote))
