19f6553de5c547a5ca0b0b73d4830aa25b79acb9
Aidar <gluhovkich@gmail.com>
2021-10-20 20:54:14 +0300

    Add a comments to route-functions

--- app/routes.py
+++ app/routes.py
@@ -7,22 +7,24 @@ from app.models import Users
 @app.route('/')
 @app.route('/home')
 def home():
+    """Возвращает страницу home"""
     return render_template('home.html', title='Home')
 
 
 @app.route('/documentation')
 def docs():
+    """Возвращает страницу с документацией"""
     return render_template('documentation.html', title='Documentation')
 
 
 @app.route('/registration', methods=['GET', 'POST'])
 def reg():
+    """Возвращает страницу с регистрацией"""
     login = LoginForm()
     if login.validate_on_submit():
         if Users.query.filter_by(username=login.username.data).first():
             flash('Пользователь под таким логином уже существует.')
             return redirect(url_for('reg'))
-        
         user = Users(user_id=Users.query.count() + 1, username=login.username.data,
                      password_hash=Users.make_password_hash(login.password.data))
         db.session.add(user)
