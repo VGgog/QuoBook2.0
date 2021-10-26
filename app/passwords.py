995d1be01fa7a502d0139e2d7aa88346b1f07cb4
Aidar <gluhovkich@gmail.com>
2021-10-26 22:03:43 +0300

    Add generate_token function

--- app/passwords.py
+++ app/passwords.py
@@ -1,3 +1,5 @@
+import random
+
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -9,3 +11,13 @@ def make_password_hash(password):
 def check_password(password_hash, password):
     """Сравнивает хэш-пароля и заданный пароль"""
     return check_password_hash(password_hash, password)
+
+
+def generate_token():
+    """Генерирует токен"""
+    SYMBLS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    token = ''
+    for i in range(32):
+        token += random.choice(SYMBLS)
+
+    return token
