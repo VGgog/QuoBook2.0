752ce110988811c591f1288f41a5df4bc411ff5e
Aidar <gluhovkich@gmail.com>
2021-10-26 20:57:50 +0300

    Chabged registartion form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+import app.forms as forms
 from app.models import Users
 from app import passwords
 
@@ -21,11 +21,16 @@ def docs():
 @app.route('/registration', methods=['GET', 'POST'])
 def reg():
     """Возвращает страницу с регистрацией"""
-    login = LoginForm()
+    login = forms.RegistrationForm()
     if login.validate_on_submit():
         if Users.query.filter_by(username=login.username.data).first():
             flash('Пользователь под таким логином уже существует.')
             return redirect(url_for('reg'))
+
+        if not login.password.data == login.password2.data:
+            flash('Пароли не совпадают.')
+            return redirect(url_for('reg'))
+
         user = Users(user_id=Users.query.count() + 1, username=login.username.data,
                      password_hash=passwords.make_password_hash(login.password.data))
         db.session.add(user)
