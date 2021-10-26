ad69a9c13e4bf455eeb2bb2abf00dc752ee2e846
Aidar <gluhovkich@gmail.com>
2021-10-26 21:01:25 +0300

    Add RegistartionForm

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,16 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
+    """Форма аутентификации пользователя"""
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    """Форма регистрации нового пользователя"""
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    password2 = PasswordField('repeat password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
 
