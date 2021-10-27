2db0bbfa5bc6105f3e3c732222320ed1b325055f
Aidar <gluhovkich@gmail.com>
2021-10-27 21:46:41 +0300

    Add check_user function

--- app/function.py
+++ app/function.py
@@ -1,5 +1,6 @@
-from app.models import Users, Quote
+from app.models import Users
 from app import passwords
+from flask import flash, redirect, url_for
 
 
 def check_password(username, password):
@@ -9,3 +10,16 @@ def check_password(username, password):
         return True
 
     return False
+
+
+def check_user(username, password, page):
+    """Проверяет существования логина и правильность введённого пароля."""
+    if not Users.query.filter_by(username=username).first():
+        flash('Такой логин не зарегистрирован.')
+        return redirect(url_for(page))
+
+    if not check_password(username, password):
+        flash('Пароль не верный.')
+        return redirect(url_for(page))
+
+    return True
