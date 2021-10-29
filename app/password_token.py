edcac562113898c82712c23b7ecd33dd64c5e9ee
Aidar <gluhovkich@gmail.com>
2021-10-29 21:34:59 +0300

    Rename and add new function in passwords module
    Rename passwords for password_token.
    In this module include functions associated with password and token.
    
    Add check_password() function.

--- app/password_token.py
+++ app/password_token.py
@@ -1,6 +1,6 @@
-import random
-
 from werkzeug.security import generate_password_hash, check_password_hash
+from app.models import Users
+import random
 
 
 def make_password_hash(password):
@@ -8,9 +8,18 @@ def make_password_hash(password):
     return generate_password_hash(password)
 
 
-def check_password(password_hash, password):
+def check_hash_password(password_hash, password):
     """Сравнивает хэш-пароля и заданный пароль"""
     return check_password_hash(password_hash, password)
+
+
+def check_password(username, password):
+    """Проверяет правильность введённого пароля"""
+    password_hash = Users.query.filter_by(username=username).first().password_hash
+    if check_hash_password(password_hash, password):
+        return True
+
+    return False
 
 
 def generate_token():
