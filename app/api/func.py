a563059601fc16bc30e37597bf81369bd4f10485
Aidar <gluhovkich@gmail.com>
2021-11-01 22:46:50 +0300

    Cut many functions
    Many functions passed many checks. Which anyway return True or False.

--- app/api/func.py
+++ app/api/func.py
@@ -1,7 +1,7 @@
 # Модуль для работы с функциями
 
 from app.models import Users, Quote
-from app import password_token
+from werkzeug.security import check_password_hash
 
 
 def translates_into_the_correct_format(info_for_quote):
@@ -20,60 +20,39 @@ def translates_into_the_correct_format(info_for_quote):
 def check_user(quote_data):
     """Производит проверку, зарегистрирован ли пользователь, и правильность введённого пароля"""
     if Users.query.filter_by(username=quote_data['login']).first():
-
-        password_hash = Users.query.filter_by(username=quote_data['login']).first().password_hash
-        if password_token.check_password(password_hash, quote_data['password']):
-            return True
-
+        return check_password_hash(Users.query.filter_by(username=quote_data['login']).first().password_hash,
+                                   quote_data['password'])
     return False
 
 
 def checking_correctness_json(quote_data):
     """Проверяет правильность отправленного json"""
-    if 'token' and 'quote' in quote_data:
-        if 'author' and 'book_title' and 'quote' in quote_data['quote']:
-            return True
-    return False
+    return ('token' and 'quote' in quote_data) and ('author' and 'book_title' and 'quote' in quote_data['quote'])
 
 
 def made_quote_obj(quote_data, quote_id):
     """Возвращает цитату в виде объекта"""
     quote_info = quote_data['quote']
     user_id = Users.query.filter_by(token=quote_data['token']).first().user_id
-
     return Quote(user_id=user_id, quote_id=quote_id,  author=quote_info['author'],
                  book_title=quote_info['book_title'], quote=quote_info['quote'])
 
 
 def check_user_id_and_quote_user_id(token, quote_id):
     """Проверяет user_id цитаты и user_id логина который отправили в запросе."""
-    quote = Quote.query.get_or_404(quote_id)
-
-    if Users.query.filter_by(token=token).first().user_id == quote.user_id:
-        return True
-
-    return False
+    return Users.query.filter_by(token=token).first().user_id == Quote.query.get_or_404(quote_id).user_id
 
 
 def checking_correct_json2(quote_data):
     """Проверяет наличие нужных полей в отправленном json-файле"""
-    if 'login' and 'password' in quote_data:
-        return True
-
-    return False
+    return 'login' and 'password' in quote_data
 
 
 def check_token(token):
     """Проверяет наличие токена в базе данных"""
-    if Users.query.filter_by(token=token).first():
-        return True
-    return False
+    return Users.query.filter_by(token=token).first()
 
 
 def check_correctness_json_with_token(quote_data):
     """Проверяет наличие токена в json и наличие токена в бд"""
-    if 'token' in quote_data:
-        if check_token(quote_data['token']):
-            return True
-
-    return False
+    return ('token' in quote_data) and (Users.query.filter_by(token=quote_data.get('token')).first())
