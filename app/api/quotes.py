146fe578a0795b4dd426a14a1bba631fb270bf29
Aidar <gluhovkich@gmail.com>
2021-11-02 20:53:21 +0300

    Rename all functions from outside moduls

--- app/api/quotes.py
+++ app/api/quotes.py
@@ -2,7 +2,7 @@ from app.api import bp
 from flask import jsonify, request
 from app.models import Quote, Users
 from app import db
-from app.api import func
+from app.api import check, quote_management
 from random import randrange, choice
 from werkzeug.security import generate_password_hash
 
@@ -10,18 +10,18 @@ from werkzeug.security import generate_password_hash
 @bp.route('/quote/<int:quote_id>', methods=['GET'])
 def send_quotes_on_quote_id(quote_id):
     """Возвращает цитату по id цитаты"""
-    return jsonify(func.translates_into_the_correct_format(Quote.query.get_or_404(quote_id)))
+    return jsonify(quote_management.return_dict_quote_info(Quote.query.get_or_404(quote_id)))
 
 
 @bp.route('/quote/<string:author_or_book_title>', methods=['GET'])
 def send_quotes_on_author_or_book_title(author_or_book_title):
     """Возвращает цитату по автору или названию книги"""
     if Quote.query.filter_by(author=author_or_book_title).first():
-        return jsonify(func.translates_into_the_correct_format(Quote.query.filter_by(
+        return jsonify(quote_management.return_dict_quote_info(Quote.query.filter_by(
             author=author_or_book_title).first()))
 
     if Quote.query.filter_by(book_title=author_or_book_title).first():
-        return jsonify(func.translates_into_the_correct_format(Quote.query.filter_by(
+        return jsonify(quote_management.return_dict_quote_info(Quote.query.filter_by(
             book_title=author_or_book_title).first()))
 
     return "Author or book title not found", 404
@@ -36,7 +36,7 @@ def send_quotes_author_and_book_title(author, book_title):
             quotes.append(quote)
 
     if quotes:
-        return jsonify(func.translates_into_the_correct_format(choice(quotes)))
+        return jsonify(quote_management.return_dict_quote_info(choice(quotes)))
 
     return "Quote not found", 404
 
@@ -50,7 +50,7 @@ def send_give_count_quotes(count):
     while i < count:
         quote_id = randrange(1, Quote.query.count())
         if quote_id not in quotes_id:
-            quotes.append(func.translates_into_the_correct_format(Quote.query.get_or_404(quote_id)))
+            quotes.append(quote_management.return_dict_quote_info(Quote.query.get_or_404(quote_id)))
             quotes_id.append(quote_id)
             i += 1
 
@@ -64,7 +64,7 @@ def send_all_quote_id_which_add_user():
     """Возвращает id всех цитат, добавленных пользователем"""
     quote_data = request.get_json() or {}
     quotes_id = []
-    if not func.check_correctness_json_with_token(quote_data):
+    if not (check.token_in_json(quote_data) and check.token_in_db(quote_data['token'])):
         return "The form of the submitted json is not correct.", 400
 
     for quote in Quote.query.filter_by(user_id=Users.query.filter_by(token=quote_data['token']).first().user_id):
@@ -79,7 +79,7 @@ def send_all_quote_id_which_add_user():
 def registration_new_user():
     """Регистрация нового пользователя"""
     quote_data = request.get_json()
-    if not func.checking_correct_json2(quote_data):
+    if not check.login_and_password_in_sent_json(quote_data):
         return "The form of the submitted json is not correct.", 400
 
     if Users.query.filter_by(username=quote_data['login']).first():
@@ -96,10 +96,10 @@ def registration_new_user():
 def changed_pass():
     """Позволяет поменять пароль"""
     quote_data = request.get_json()
-    if not func.checking_correct_json2(quote_data) and ('new_password' in quote_data):
+    if not check.login_and_password_in_sent_json(quote_data) and ('new_password' in quote_data):
         return "The form of the submitted json is not correct.", 400
 
-    if not func.check_user(quote_data):
+    if not check.user_login_and_password(quote_data):
         return "Login or password is incorrect", 401
 
     if not Users.query.filter_by(username=quote_data['login']).first():
@@ -117,9 +117,9 @@ def add_new_quote():
     quote_id = request.args.get('quote_id', type=int) or None
     quote_data = request.get_json() or {}
 
-    if not func.checking_correctness_json(quote_data):
+    if not check.correct_form_sent_json(quote_data):
         return "The form of the submitted json is not correct.", 400
-    if not func.check_token(quote_data['token']):
+    if not check.token_in_db(quote_data['token']):
         return "Token is incorrect", 401
 
     quote_text = quote_data['quote']['quote']
@@ -127,17 +127,17 @@ def add_new_quote():
         return "This quote already added.", 404
 
     if quote_id:
-        if not func.check_user_id_and_quote_user_id(quote_data['token'], quote_id):
+        if not check.user_and_quote_user_id(quote_data['token'], quote_id):
             return "You do not have permission to update this quote.", 403
         if Quote.query.filter_by(quote_id=quote_id).first():
             db.session.delete(Quote.query.filter_by(quote_id=quote_id).first())
-        quote = func.made_quote_obj(quote_data, quote_id=quote_id)
+        quote = quote_management.creates_a_quote_object(quote_data, quote_id=quote_id)
     else:
-        quote = func.made_quote_obj(quote_data, quote_id=Quote.query.count() + 1)
+        quote = quote_management.creates_a_quote_object(quote_data, quote_id=Quote.query.count() + 1)
 
     db.session.add(quote)
     db.session.commit()
-    return jsonify(func.translates_into_the_correct_format(
+    return jsonify(quote_management.return_dict_quote_info(
         Quote.query.filter_by(quote=quote_text).first())), 200
 
 
@@ -145,13 +145,13 @@ def add_new_quote():
 def delete_user():
     """Удаляет пользователя"""
     quote_data = request.get_json()
-    if not func.checking_correct_json2(quote_data):
+    if not check.login_and_password_in_sent_json(quote_data):
         return "The form of the submitted json is not correct.", 400
 
     if not Users.query.filter_by(username=quote_data['login']).first():
         return "This login not found", 404
 
-    if not func.check_user(quote_data):
+    if not check.user_login_and_password(quote_data):
         return "Login or password is incorrect", 401
 
     db.session.delete(Users.query.filter_by(username=quote_data['login']).first())
@@ -164,13 +164,13 @@ def delete_quote(quote_id):
     """Delete-метод, удаляет цитату, если Вы её добавляли."""
     quote_data = request.get_json() or {}
 
-    if not func.check_correctness_json_with_token(quote_data):
+    if not (check.token_in_json(quote_data) and check.token_in_db(quote_data['token'])):
         return "The form or the token of the submitted json is not correct.", 400
 
-    if not func.check_user_id_and_quote_user_id(quote_data['token'], quote_id):
+    if not check.user_and_quote_user_id(quote_data['token'], quote_id):
         return "You do not have permission to delete this quote.", 403
 
     quote = Quote.query.filter_by(quote_id=quote_id).first()
     db.session.delete(Quote.query.filter_by(quote_id=quote_id).first())
     db.session.commit()
-    return jsonify(func.translates_into_the_correct_format(quote)), 200
+    return jsonify(quote_management.return_dict_quote_info(quote)), 200
