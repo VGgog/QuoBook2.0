3be9b666febf23e57983b0a883f422ffe4d49df0
Aidar <gluhovkich@gmail.com>
2021-11-08 15:53:28 +0300

    Initialization tests packet and add db
    Add __init__.py module in which there is nothing.
    Add in tes_app.py setUp which creat db, and tearDown which delete db
    when test ends.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,25 @@
-from app import app
+from app import app, db
+from app.models import Users, Quote
 import unittest
 
 
-class BasicTestCase(unittest.TestCase):
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:fqlfh2004@localhost:5432/QuoBookTest"
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
     def test_home(self):
         tester = app.test_client(self)
-        response = tester.get('/')
+        response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
     def test_docs(self):
         tester = app.test_client(self)
-        response = tester.get('/documentation')
+        response = tester.get('/documentation', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
 
