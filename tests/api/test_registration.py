92c28de3708934d39e094d0882e37af40647e040
Aidar <gluhovkich@gmail.com>
2021-11-11 20:45:11 +0300

    Added additional test

--- tests/api/test_registration.py
+++ tests/api/test_registration.py
@@ -1,4 +1,4 @@
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import Users
 from app import app, db
 import unittest
@@ -29,28 +29,37 @@ class AppTestCase(unittest.TestCase):
         response = self.tester.post('/api/registration', data=json.dumps({
             'login': 'papatola', 'password': 'pororo'}), content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('You have successful registered', response.get_data(as_text=True))
+        self.assertEqual(Users.query.filter_by(username='papatola').first().token, response.get_data(as_text=True))
+        user_data = Users.query.filter_by(username='papatola').first()
+        self.assertEqual(user_data.username, 'papatola')
+        self.assertTrue(check_password_hash(user_data.password_hash, 'pororo'))
 
     def test_json_without_login(self):
         """Регистрация, когда отправленный json без логина"""
         response = self.tester.post('/api/registration', data=json.dumps({
             'password': 'pororo'}), content_type='application/json')
         self.assertEqual(response.status_code, 400)
-        self.assertIn('The form of the submitted json is not correct.', response.get_data(as_text=True))
+        self.assertEqual('The form of the submitted json is not correct.', response.get_data(as_text=True))
 
     def test_json_without_password(self):
         """Отправленный json без пароля"""
         response = self.tester.post('/api/registration', data=json.dumps({
             'login': 'papatola'}), content_type='application/json')
         self.assertEqual(response.status_code, 400)
-        self.assertIn('The form of the submitted json is not correct.', response.get_data(as_text=True))
+        self.assertEqual('The form of the submitted json is not correct.', response.get_data(as_text=True))
 
     def test_login_in_exists(self):
         """Проверяет поведение программы, если login уже есть в бд"""
         response = self.tester.post('/api/registration', data=json.dumps({
             'login': 'monoliza', 'password': 'igrauchu'}), content_type='application/json')
         self.assertEqual(response.status_code, 401)
-        self.assertIn('A user with this username already exists', response.get_data(as_text=True))
+        self.assertEqual('A user with this username already exists', response.get_data(as_text=True))
+
+    def test_json_is_not_sent(self):
+        """Тест, без отправленного json"""
+        response = self.tester.post('/api/registration')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual('The form of the submitted json is not correct.', response.get_data(as_text=True))
 
 
 if __name__ == '__main__':
