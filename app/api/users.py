8c4849774559145e977bc3f1a527e9e374c0a2c3
Aidar <gluhovkich@gmail.com>
2021-11-14 21:33:48 +0300

    Add a user_authentication() and write tests for it

--- app/api/users.py
+++ app/api/users.py
@@ -1,8 +1,8 @@
 from werkzeug.security import generate_password_hash
+from flask import request, jsonify
 from app import generate_token
 from app.models import Users
 from app.api import check
-from flask import request
 from app.api import bp
 from app import db
 
@@ -24,38 +24,17 @@ def registration_new_user():
     return user.token, 200
 
 
-@bp.route('/changed_password', methods=['PUT'])
-def changed_pass():
-    """Позволяет поменять пароль"""
-    quote_data = request.get_json()
-    if not check.login_and_password_in_sent_json(quote_data) and ('new_password' in quote_data):
+@bp.route('/authentication', methods=['POST'])
+def user_authentication():
+    """Авторизация пользователя, возвращает словарь {"token": <Токен>}"""
+    user_data = request.get_json()
+    if not check.login_and_password_in_sent_json(user_data):
         return "The form of the submitted json is not correct.", 400
 
-    if not check.user_login_and_password(quote_data):
-        return "Login or password is incorrect", 401
-
-    if not Users.query.filter_by(username=quote_data['login']).first():
+    if not Users.query.filter_by(username=user_data['login']).first():
         return "This login not found", 404
 
-    user = Users.query.filter_by(username=quote_data['login']).first()
-    user.password_hash = generate_password_hash(quote_data['new_password'])
-    db.session.commit()
-    return "You have successful changed password", 200
-
-
-@bp.route('/del_user', methods=['DELETE'])
-def delete_user():
-    """Удаляет пользователя"""
-    quote_data = request.get_json()
-    if not check.login_and_password_in_sent_json(quote_data):
-        return "The form of the submitted json is not correct.", 400
-
-    if not Users.query.filter_by(username=quote_data['login']).first():
-        return "This login not found", 404
-
-    if not check.user_login_and_password(quote_data):
+    if not check.user_login_and_password(user_data):
         return "Login or password is incorrect", 401
 
-    db.session.delete(Users.query.filter_by(username=quote_data['login']).first())
-    db.session.commit()
-    return "This user successful delete", 200
+    return jsonify({'token': Users.query.filter_by(username=user_data['login']).first().token}), 200
---
+++ tests/api/test_user_authentication.py
@@ -0,0 +1,61 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import generate_token
+from app.models import Users
+from app import app, db
+import unittest
+import json
+
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.tester = app.test_client()
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:fqlfh2004@localhost:5432/QuoBookTest"
+
+        # Добавляет пользователя в бд, для проведения теста в методе test_login_in_exists()
+        db.session.add(Users(user_id=1, username='monoliza', password_hash=generate_password_hash('igrauchu'),
+                             token=generate_token.generate_token()))
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_successful(self):
+        """"""
+        response = self.tester.post('/api/authentication', data=json.dumps({
+            'login': 'monoliza', 'password': 'igrauchu'}), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("token", response.get_data(as_text=True))
+
+    def test_error1(self):
+        """Успешная регистрация"""
+        response = self.tester.post('/api/authentication', data=json.dumps({
+            'login': 'monoliza'}), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual("The form of the submitted json is not correct.", response.get_data(as_text=True))
+
+    def test_error2(self):
+        """Успешная регистрация"""
+        response = self.tester.post('/api/authentication', data=json.dumps({
+            'password': 'igrauchu'}), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual("The form of the submitted json is not correct.", response.get_data(as_text=True))
+
+    def test_error3(self):
+        """Успешная регистрация"""
+        response = self.tester.post('/api/authentication', data=json.dumps({
+            'login': 'igoryan', 'password': 'igrauchu'}), content_type='application/json')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual("This login not found", response.get_data(as_text=True))
+
+    def test_error4(self):
+        """Успешная регистрация"""
+        response = self.tester.post('/api/authentication', data=json.dumps({
+            'login': 'monoliza', 'password': 'igrauchu345'}), content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual("Login or password is incorrect", response.get_data(as_text=True))
+
+
+if __name__ == '__main__':
+    unittest.main()
