c92eb14d0970c7e0ff811de5b7069fb85ea808e3
Aidar <gluhovkich@gmail.com>
2022-02-03 23:54:03 +0500

    Delete logging

--- app/__init__.py
+++ app/__init__.py
@@ -15,22 +15,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 app.config['JSON_AS_ASCII'] = False
 
-if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
-
 
 from app.api import bp as api_bp
 app.register_blueprint(api_bp, url_prefix='/api')
--- config.py
+++ config.py
@@ -2,12 +2,6 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('MY_SECRET_KEY')
+    SECRET_KEY = os.environ.get('MY_SECRET_KEY') or "my-secret-key"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ["gluhovkich@gmail.com"]
